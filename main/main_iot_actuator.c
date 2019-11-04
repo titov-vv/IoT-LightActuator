b8dff6e6939163a0c8bc00539bf6a977c8b7d34b
titov-vv <55182132+titov-vv@users.noreply.github.com>
2019-11-04 16:38:34 +0300

    main.c cleanup

--- main/main_iot_actuator.c
+++ main/main_iot_actuator.c
@@ -5,21 +5,19 @@
 	- Get state from Shadow
 	- Power on/off connected lamp
 */
-//https://github.com/espressif/esp-aws-iot/blob/master/examples/thing_shadow/main/thing_shadow_sample.c
-//https://github.com/espressif/esp-aws-iot/blob/master/examples/subscribe_publish/main/subscribe_publish_sample.c
-#include <stdio.h>
-
+//-----------------------------------------------------------------------------
+// FreeRTOS
 #include "../build/config/sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
-
+// Espressif
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 #include "esp_event.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
-
+// Project
 #include "blink.h"
 #include "wifi.h"
 #include "thing.h"
@@ -29,11 +27,7 @@
 //-----------------------------------------------------------------------------
 // Pin connected to MOSFET key that operates relay
 #define LAMP_PIN			GPIO_NUM_13
-
 //-----------------------------------------------------------------------------
-//static bool	LampIsOn = false;
-//-----------------------------------------------------------------------------
-
 /* FreeRTOS event group to to check signals */
 static EventGroupHandle_t events_group;
 const int IP_UP_BIT = BIT0;	// Bit to check IP link readiness
@@ -72,116 +66,6 @@ const int STATE_BIT = BIT1; // Bit is set if state was changed
 //        }
 //        xEventGroupSetBits(events_group, STATE_BIT);
 //    }
-//}
-////-----------------------------------------------------------------------------
-//void aws_iot_task(void *arg)
-//{
-//    IoT_Error_t rc = FAILURE;
-//    AWS_IoT_Client aws_client;
-//    ShadowInitParameters_t shadowParams = ShadowInitParametersDefault;
-//    ShadowConnectParameters_t connectParams = ShadowConnectParametersDefault;
-//
-//    bool lampOn = false;
-//    jsonStruct_t lamp_actuator;
-//    lamp_actuator.cb = lamp_actuate_callback;
-//    lamp_actuator.pData = &lampOn;
-//    lamp_actuator.pKey = "lampOn";
-//    lamp_actuator.type = SHADOW_JSON_BOOL;
-//    lamp_actuator.dataLength = sizeof(bool);
-//
-//    ESP_LOGI(TAG_AWS, "Shadow init started");
-//    shadowParams.enableAutoReconnect = false;
-//    shadowParams.pHost = AWS_host;
-//    shadowParams.port = AWS_port;
-//    shadowParams.pRootCA  = aws_root_ca_pem;
-//    shadowParams.pClientCRT  = certificate_pem_crt;
-//    shadowParams.pClientKey  = private_pem_key;
-//    shadowParams.disconnectHandler = aws_disconnect_handler;
-//
-//    rc = aws_iot_shadow_init(&aws_client, &shadowParams);
-//    if (rc != SUCCESS)
-//    {
-//        ESP_LOGE(TAG_AWS, "Shadow init failure: %d ", rc);
-//        vTaskDelete(NULL);
-//        return;
-//    }
-//    ESP_LOGI(TAG_AWS, "Shadow init success");
-//
-//    ESP_LOGI(TAG_AWS, "Wait for IP link");
-//    xEventGroupWaitBits(events_group, IP_UP_BIT, false, true, portMAX_DELAY);
-//    ESP_LOGI(TAG_AWS, "IP link is up");
-//
-//    ESP_LOGI(TAG_AWS, "Shadow connect started");
-//    connectParams.pMyThingName = AWS_CLIENTID;
-//    connectParams.pMqttClientId = AWS_CLIENTID;
-//    connectParams.mqttClientIdLen = (uint16_t) strlen(AWS_CLIENTID);
-//    do
-//    {
-//        rc = aws_iot_shadow_connect(&aws_client, &connectParams);
-//        if(rc != SUCCESS)
-//        {
-//            ESP_LOGE(TAG_AWS, "Shadow connect error: %d", rc);
-//            vTaskDelay(5000 / portTICK_RATE_MS);
-//        }
-//    }
-//    while(rc != SUCCESS);
-//    ESP_LOGI(TAG_AWS, "Shadow connected");
-//
-//    rc = aws_iot_mqtt_autoreconnect_set_status(&aws_client, true);
-//    if(rc == SUCCESS)
-//    	ESP_LOGI(TAG_AWS, "Shadow auto-reconnect enabled");
-//    else
-//    	ESP_LOGE(TAG_AWS, "Shadow auto-reconnect setup failure: %d ", rc);
-//
-//    rc = aws_iot_shadow_register_delta(&aws_client, &lamp_actuator);
-//    if(rc == SUCCESS)
-//        ESP_LOGI(TAG_AWS, "Shadow Delta callback registered");
-//    else
-//     	ESP_LOGE(TAG_AWS, "Shadow Delta callback failed: %d ", rc);
-//
-//
-//    while(1)
-//    {
-//    	rc = aws_iot_shadow_yield(&aws_client, 100);
-//        if (rc != SUCCESS)
-//        	continue;
-//
-//        if (xEventGroupGetBits(events_group) & STATE_BIT)
-//        {
-//
-////    Update shadow state
-////        	{
-////        	  "state": {
-////        	    "reported": {
-////        	      "lampOn": false
-////        	    }
-////        	  }
-////        	}
-//        	ESP_LOGI(TAG_AWS, "Report state");
-//        	root = cJSON_CreateObject();
-//        	state = cJSON_CreateObject();
-//        	cJSON_AddItemToObject(root, "state", state);
-//        	reported = cJSON_CreateObject();
-//        	cJSON_AddItemToObject(state, "reported", reported);
-//        	cJSON_AddBoolToObject(reported, "lampOn", LampIsOn);
-//
-//        	if (!cJSON_PrintPreallocated(root, JSON_buffer, MAX_JSON_SIZE, 0 /*  not formatted */))
-//        	{
-//        		ESP_LOGW(TAG_AWS, "JSON buffer too small");
-//        	    JSON_buffer[0] = 0;
-//        	}
-//        	cJSON_Delete(root);
-//        	ESP_LOGI(TAG_AWS, "JSON Thing reported state: %s", JSON_buffer);
-//
-//        	rc = aws_iot_shadow_update(&aws_client, AWS_CLIENTID, JSON_buffer, shadow_update_callback, NULL, 4, true);
-//
-//        	xEventGroupClearBits(events_group, STATE_BIT);
-//        }
-//
-//        vTaskDelay(1000 / portTICK_RATE_MS);
-//    }
-//
-//	vTaskDelete(NULL);
 //}
 //-----------------------------------------------------------------------------
 void app_main(void)
